d6d74796eda9bb193c637725b85496c6d03362d8
tiendungcln <tiendung0301cln@gmail.com>
2026-06-13 16:31:20 +0700

    update Level_3

--- src/Level_3.java
+++ src/Level_3.java
@@ -7,11 +7,35 @@ public class Level_3 {
     // 9
     // 12
 
-    public
+    public static void printWindowSums(int[] arr, int k){
+
+        int sum = 0;
+
+        for (int i = 0; i < k; i++){
+
+            sum += arr[i];
+
+        }
+
+        System.out.println(sum);
+
+        for (int i = k; i < arr.length; i++){
+
+            // sum mới = sum cũ - vị trí bị đẩy ra + vị trí được thêm vào
+            sum = sum - arr[i - k] + arr[i];
+
+            System.out.println(sum);
+
+        }
+
+    }
 
     public static void main(String[] args){
 
+        int[] arr = {1, 2, 3, 4, 5};
+        int k = 3;
 
+       printWindowSums(arr, k);;
 
     }
 
