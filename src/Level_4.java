85565650eec6edae4a7e5dc32cab5487a43faf9a
tiendungcln <tiendung0301cln@gmail.com>
2026-06-13 17:07:15 +0700

    update Level_4

---
+++ src/Level_4.java
@@ -0,0 +1,44 @@
+public class Level_4 {
+    // Tìm tổng lớn nhất
+    // Cho:[1,2,3,4,5]
+    // k = 3
+    // Kết quả:
+    // 12
+
+    public static int maxSumWindow(int[] arr, int k){
+
+        int sum = 0;
+
+        for (int i = 0; i < k; i++){
+
+            sum += arr[i];
+
+        }
+
+        int maxSum = sum;
+
+        for (int i = k; i < arr.length; i++){
+
+            sum += arr[i] - arr[i - k];
+
+            if (sum > maxSum){
+
+                maxSum = sum;
+
+            }
+
+        }
+
+        return maxSum;
+
+    }
+
+    public static void main(String[] args){
+
+        int[] arr = {5, 2, -1, 0, 3};
+        int k = 3;
+
+        System.out.println(maxSumWindow(arr, k));;
+
+    }
+}
