d58be183af7558d57834436e6b4cc99f97a0ed62
tiendungcln <tiendung0301cln@gmail.com>
2026-06-13 15:35:23 +0700

    update Level_2

---
+++ src/Level_3.java
@@ -0,0 +1,18 @@
+public class Level_3 {
+    // In tổng từng cửa sổ
+    // Cho:[1,2,3,4,5]
+    // k = 3
+    // In:
+    // 6
+    // 9
+    // 12
+
+    public
+
+    public static void main(String[] args){
+
+
+
+    }
+
+}
